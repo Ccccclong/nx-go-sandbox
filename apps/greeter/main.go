2065609b10a8e6e947ebdcc37d49fa09815d0679
Chan Sze Long <szelong.chan@sandboxvr.com>
2022-10-11 11:35:45 +0800

    chore(greeter): import speaker

--- apps/greeter/main.go
+++ apps/greeter/main.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/ccccclong/nx-go-sandbox/libs/speaker/cat"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Hello! Whats your name?")
 	name, _ := reader.ReadString('\n')
-	fmt.Println("Nice to meet you", name)
+	fmt.Print(cat.Speak("Nice to meet you " + name))
 }
