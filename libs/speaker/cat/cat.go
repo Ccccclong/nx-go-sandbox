190aeadd0f80b6fd406a6370884c0b2f77786a88
Chan Sze Long <szelong.chan@sandboxvr.com>
2022-10-11 11:32:55 +0800

    fix(cat): read txt file properly

--- libs/speaker/cat/cat.go
+++ libs/speaker/cat/cat.go
@@ -1,11 +1,27 @@
 package cat
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
+	"runtime"
 )
 
 func Speak(message string) string {
-	content, _ := os.ReadFile("cat.txt")
+	dirname := getDirname()
+	content, err := os.ReadFile(dirname + "/cat.txt")
+	if err != nil {
+		panic(err)
+	}
 	return fmt.Sprintf(string(content), message)
+}
+
+func getDirname() string {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		panic(errors.New("unable to get the current filename"))
+	}
+	dirname := filepath.Dir(filename)
+	return dirname
 }
